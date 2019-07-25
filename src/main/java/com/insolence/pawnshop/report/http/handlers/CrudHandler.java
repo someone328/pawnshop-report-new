79b016f271f41174defa89a42b8f5e825bbee660
Denis R <someone328@gmail.com>
2019-07-25 22:26:15 +0300

    fix get. it returns password too

--- src/main/java/com/insolence/pawnshop/report/http/handlers/CrudHandler.java
+++ src/main/java/com/insolence/pawnshop/report/http/handlers/CrudHandler.java
@@ -9,97 +9,98 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CrudHandler implements Handler<RoutingContext> {
 
-  @Override
-  public void handle(RoutingContext rc) {
-    String objectType = rc.request().getParam("objectType");
-    String operationType = rc.request().getParam("operationType");
-    Operarions operationTypeEnum;
-    
-    try {
-    	SupportedObjectTypes.valueOf(objectType.toUpperCase());
-    } catch (Exception e) {
-    	rc.response().setStatusCode(400).end("Unsupported object type " + objectType);
-    	return;
+    @Override
+    public void handle(RoutingContext rc) {
+        String objectType = rc.request().getParam("objectType");
+        String operationType = rc.request().getParam("operationType");
+        Operarions operationTypeEnum;
+
+        try {
+            SupportedObjectTypes.valueOf(objectType.toUpperCase());
+        } catch (Exception e) {
+            rc.response().setStatusCode(400).end("Unsupported object type " + objectType);
+            return;
+        }
+
+        try {
+            operationTypeEnum = Operarions.valueOf(operationType.toUpperCase());
+        } catch (Exception e) {
+            rc.response().setStatusCode(400).end("Unsupported operationType type " + operationType);
+            return;
+        }
+
+        DeliveryOptions deliveryOptions = new DeliveryOptions().addHeader("objectType", objectType);
+        JsonObject jsonBody = rc.getBodyAsJson();
+
+        switch (operationTypeEnum) {
+            case PUT -> processPutOperation(rc, deliveryOptions, jsonBody);
+            case GET -> processGetOperation(rc, deliveryOptions, jsonBody);
+            case DELETE -> processDeleteOperation(rc, deliveryOptions, jsonBody);
+            default -> rc.response().setStatusCode(400).end("Unsupported operationType type " + operationType);
+        }
+
     }
-    
-    try {
-    	operationTypeEnum=Operarions.valueOf(operationType.toUpperCase());
-    } catch (Exception e){
-    	rc.response().setStatusCode(400).end("Unsupported operationType type " + operationType);
-    	return;
+
+    private void processDeleteOperation(RoutingContext rc, DeliveryOptions deliveryOptions, JsonObject jsonBody) {
+        String idToDelete = jsonBody.getString("_id");
+        log.info(idToDelete);
+        rc.vertx()
+                .eventBus()
+                .rxSend("crud.delete", idToDelete, deliveryOptions)
+                .map(responce -> responce.body().toString())
+                .subscribe(success -> rc.response().setStatusCode(200).end(success),
+                        error -> {
+                            log.error("Process DELETE error", error);
+                            rc.response().setStatusCode(500).end();
+                        });
     }
-    
-    DeliveryOptions deliveryOptions = new DeliveryOptions().addHeader("objectType", objectType);
-    JsonObject jsonBody = rc.getBodyAsJson();
-    
-    switch (operationTypeEnum) {
-    	case PUT -> processPutOperation(rc,deliveryOptions,jsonBody);
-    	case GET -> processGetOperation(rc,deliveryOptions,jsonBody);
-    	case DELETE -> processDeleteOperation(rc,deliveryOptions,jsonBody);
-    	default -> rc.response().setStatusCode(400).end("Unsupported operationType type " + operationType);
+
+    private void processGetOperation(RoutingContext rc, DeliveryOptions deliveryOptions, JsonObject jsonBody) {
+        rc.vertx()
+                .eventBus()
+                .rxSend("crud.get", jsonBody, deliveryOptions)
+                .map(response -> response.body().toString())
+                .subscribe(
+                        crudResult ->
+                                rc.response()
+                                        .putHeader("content-type", "application/json; charset=utf-8")
+                                        .end(crudResult),
+                        error -> {
+                            log.error("Crud handler error. ", error);
+                            rc.fail(500);
+                        });
     }
-    
-  }
-  
-  private void processDeleteOperation(RoutingContext rc,DeliveryOptions deliveryOptions,JsonObject jsonBody){
-	  String idToDelete = jsonBody.getString("_id");
-	  log.info(idToDelete);
-	  rc.vertx()
-	  	.eventBus()
-	  	.rxSend("crud.delete", idToDelete, deliveryOptions)
-	  	.map(responce -> responce.body().toString())
-	  	.subscribe(success -> rc.response().setStatusCode(200).end(success), 
-	  			   error -> {
-	  				   log.error("Process DELETE error", error);
-	  				   rc.response().setStatusCode(500).end();
-	  			   });
-}
 
-private void processGetOperation(RoutingContext rc,DeliveryOptions deliveryOptions,JsonObject jsonBody) {
-	rc.vertx()
-	    .eventBus()
-	    .rxSend("crud.get", jsonBody, deliveryOptions)
-	    .map(response -> response.body().toString())
-	    .subscribe(
-	        crudResult ->
-	            rc.response()
-	                .putHeader("content-type", "application/json; charset=utf-8")
-	                .end(crudResult),
-	        error -> {
-	        	log.error("Crud handler error. ", error);
-	        	rc.fail(500);
-	        });
-  }
+    private void processPutOperation(RoutingContext rc, DeliveryOptions deliveryOptions, JsonObject jsonBody) {
+        rc.vertx()
+                .eventBus()
+                .rxSend("crud.put", jsonBody, deliveryOptions)
+                .map(response -> response.body().toString())
+                .subscribe(
+                        crudResult ->
+                                rc.response()
+                                        .putHeader("content-type", "application/json; charset=utf-8")
+                                        .end(crudResult),
+                        error -> {
+                            log.error("Crud handler error. ", error);
+                            rc.fail(500);
+                        });
+    }
 
-  private void processPutOperation(RoutingContext rc,DeliveryOptions deliveryOptions,JsonObject jsonBody){
-	rc.vertx()
-	    .eventBus()
-	    .rxSend("crud.put", jsonBody, deliveryOptions)
-	    .map(response -> response.body().toString())
-	    .subscribe(
-	        crudResult ->
-	            rc.response()
-	                .putHeader("content-type", "application/json; charset=utf-8")
-	                .end(crudResult),
-	        error -> {
-	        	log.error("Crud handler error. ", error);
-	        	rc.fail(500);
-	        });
-	}
+    public boolean isSupportableType(String type) {
+        return SupportedObjectTypes.valueOf(type.toUpperCase()).ordinal() != 9999;
+    }
 
-  public boolean isSupportableType(String type) {
-    return SupportedObjectTypes.valueOf(type.toUpperCase()).ordinal() != 9999;
-  }
+    public enum SupportedObjectTypes {
+        BRANCH,
+        USER,
+        REPORT,
+        ROLE
+    }
 
-  public enum SupportedObjectTypes {
-	BRANCH,
-	USER,
-    REPORT
-  }
-
-  public enum Operarions {
-    PUT,
-    GET,
-    DELETE
-  }
+    public enum Operarions {
+        PUT,
+        GET,
+        DELETE
+    }
 }
--- src/main/java/com/insolence/pawnshop/report/verticles/CrudVerticle.java
+++ src/main/java/com/insolence/pawnshop/report/verticles/CrudVerticle.java
@@ -9,73 +9,77 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CrudVerticle extends AbstractVerticle {
-  private MongoClient client;
+    private MongoClient client;
 
-  @Override
-  public void start() throws Exception {
-    client = MongoClient.createShared(vertx, new JsonObject(), "pawnshop-report");
+    @Override
+    public void start() throws Exception {
+        client = MongoClient.createShared(vertx, new JsonObject(), "pawnshop-report");
 
-    vertx
-        .eventBus()
-        .consumer("crud.delete")
-        .toFlowable()
-        .flatMap(
-            message ->
-                client
-                    .rxRemoveDocument(
-                        message.headers().get("objectType"),
-                        new JsonObject().put("_id", (String) message.body()))
-                    .doOnError(error -> message.fail(500, error.getMessage()))
-                    .flatMap(result -> message.rxReply(result.toJson()))
-                    .toFlowable())
-        .retry()
-        .subscribe(r -> {}, error -> log.error("crud error", error));
+        vertx
+                .eventBus()
+                .consumer("crud.delete")
+                .toFlowable()
+                .flatMapSingle(
+                        message ->
+                                client
+                                        .rxRemoveDocument(
+                                                message.headers().get("objectType"),
+                                                new JsonObject().put("_id", (String) message.body()))
+                                        .doOnError(error -> message.fail(500, error.getMessage()))
+                                        .flatMap(result -> message.rxReply(result.toJson())))
+                .retry()
+                .subscribe(r -> {
+                        },
+                        error -> log.error("crud error", error));
 
-    vertx
-        .eventBus()
-        .consumer("crud.put")
-        .toFlowable()
-        .flatMap(
-            message -> {
-              JsonObject body = (JsonObject) message.body();
-              Maybe<String> upsert;
-              if (body.containsKey("_id")) {
-                upsert =
-                    client
-                        .rxSave(message.headers().get("objectType"), body)
-                        .defaultIfEmpty(body.getString("_id"));
-              } else {
-                upsert = client.rxSave(message.headers().get("objectType"), body);
-              }
-              return upsert.flatMapPublisher(res -> message.rxReply(res).toFlowable());
-            })
-        .retry()
-        .subscribe(r -> {}, error -> log.error("crud error", error));
-
-    vertx
-        .eventBus()
-        .consumer("crud.get")
-        .toFlowable()
-        .flatMap(
-            message ->
-                client
-                    .findBatch(message.headers().get("objectType"), (JsonObject) message.body())
-                    .toFlowable()
-                    .map(
-                        object -> {
-                          object.remove("password");
-                          return object;
+        vertx
+                .eventBus()
+                .consumer("crud.put")
+                .toFlowable()
+                .flatMapSingle(
+                        message -> {
+                            JsonObject body = (JsonObject) message.body();
+                            Maybe<String> upsert;
+                            if (body.containsKey("_id")) {
+                                upsert =
+                                        client
+                                                .rxSave(message.headers().get("objectType"), body)
+                                                .defaultIfEmpty(body.getString("_id"));
+                            } else {
+                                upsert = client.rxSave(message.headers().get("objectType"), body);
+                            }
+                            return upsert.flatMapSingle(res -> message.rxReply(res));
                         })
-                    .collectInto(new JsonArray(), (array, object) -> array.add(object))
-                    .doOnError(
-                        error -> {
-                          log.error("crud GET error", error);
-                          message.fail(500, error.getMessage());
-                        })
-                    .flatMapPublisher(array -> message.rxReply(array).toFlowable()))
-        .retry()
-        .subscribe(r -> {}, error -> log.error("crud error", error));
+                .retry()
+                .subscribe(r -> {
+                        },
+                        error -> log.error("crud error", error));
 
-    log.info("CrudVerticle Ready!");
-  }
+        vertx
+                .eventBus()
+                .consumer("crud.get")
+                .toFlowable()
+                .flatMap(
+                        message ->
+                                client
+                                        .findBatch(message.headers().get("objectType"), (JsonObject) message.body())
+                                        .toFlowable()
+                                        /*.map(
+                                                object -> {
+                                                    object.remove("password");
+                                                    return object;
+                                                })*/
+                                        .collectInto(new JsonArray(), (array, object) -> array.add(object))
+                                        .doOnError(
+                                                error -> {
+                                                    log.error("crud GET error", error);
+                                                    message.fail(500, error.getMessage());
+                                                })
+                                        .flatMapPublisher(array -> message.rxReply(array).toFlowable()))
+                .retry()
+                .subscribe(r -> {
+                }, error -> log.error("crud error", error));
+
+        log.info("CrudVerticle Ready!");
+    }
 }
